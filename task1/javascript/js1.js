d2b0ecacfbb1e1353433079d2de413949263c935
admin <you@example.com>
2019-05-20 16:41:49 +0530

    commited

---
+++ task1/javascript/js1.js
@@ -0,0 +1,3 @@
+
+var a='welcome to "Javascript"'; 
+document.write(a);
---
+++ task1/javascript/js2.js
@@ -0,0 +1,5 @@
+var a="Hello world"; b="Welcome</br>"; c="Javascript"; 
+var d="Hii good afternoon";
+var e="Had your lunch";
+document.write(a+"</br>"+ b +c+"</br>")
+document.write(d+' '+e);
