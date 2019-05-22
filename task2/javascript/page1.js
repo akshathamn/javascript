a96803d15d67230a6e939197fe7f63047a1910a0
admin <you@example.com>
2019-05-22 09:20:24 +0530

    commited

---
+++ task2/javascript/page1.js
@@ -0,0 +1,13 @@
+var a=10;
+var b=20;
+ var z=a+b;
+ var c=a-b;
+ var d=a*b;
+ var e=a/b;
+document.write("First No =" +a+"</br>" );
+document.write("second no="+b+"</br>");
+document.write(a+"+"+b+"="+z+"</br>");
+document.write(a+"+"+b+"="+c+"</br>");
+document.write(a+"+"+b+"="+d+"</br>");
+document.write(a+"+"+b+"="+e+"</br>");
+
---
+++ task2/javascript/page2.js
@@ -0,0 +1,8 @@
+var jm=30;
+var jh=10;
+var mm=40;
+var mh=20;
+var mbmi=40/(20*20);
+var jbmi=30/(10*10);
+var boolean=mbmi>jbmi;
+console.log(boolean);
