c83dc958b575049a4c14bbd97028719afdd519bf
admin <you@example.com>
2019-05-22 11:43:53 +0530

    commited

--- task2/javascript/page2.js
+++ task2/javascript/page2.js
@@ -1,8 +1,10 @@
-var jm=30;
-var jh=10;
-var mm=40;
-var mh=20;
-var mbmi=40/(20*20);
-var jbmi=30/(10*10);
+var jm=40;
+var jh=20;
+var mm=30;
+var mh=10;
+var mbmi=30/(10*10);
+document.write("mark's bmi="+mbmi+"</br>")
+var jbmi=40/(20*20);
+document.write("john's bmi="+jbmi)
 var boolean=mbmi>jbmi;
 console.log(boolean);
