3aafd9f25507db2695c91d09de6cc602e939d6b4
admin <you@example.com>
2019-05-27 16:27:28 +0530

    commited

---
+++ task5/javascript/page1.js
@@ -0,0 +1,7 @@
+function longest_string(str_ara) {
+  var max = str_ara[0].length;
+  str_ara.map(v => max = Math.max(max, v.length));
+  result = str_ara.filter(v => v.length == max);
+  return result;
+}
+document.write(longest_string(['rose', 'apple', 'mango','jackfruit','africa']))
