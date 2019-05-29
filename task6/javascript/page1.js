1b5f320d8bb63421b213ebcd8526ae1776656707
admin <you@example.com>
2019-05-29 10:58:48 +0530

    commited

--- task6/javascript/page1.js
+++ task6/javascript/page1.js
@@ -1,22 +1,27 @@
-function checked(){
-  debugger;
-  var items=document.getElementsByName('check');
-  for(var i=0;i<items.length;i++)
-  {
-    if(items[i].type=='checkbox')
-    items[i].checked=true;
-    }
-  }
-
-function UpdateCost() {
+function selectAll(){
+        var items=document.getElementsByName('check');
+        for(var i=0; i<items.length; i++){
+          if(items[i].type=='checkbox')
+            items[i].checked=true;
+        }
+      }
+      
+      function UnSelectAll(){
+        var items=document.getElementsByName('check');
+        for(var i=0; i<items.length; i++){
+          if(items[i].type=='checkbox')
+            items[i].checked=false;
+        }
+      }   
+ function UpdateCost() {
   var sum = 0;
   var Numbers, elem;
-  for (i=0; i<3; i++) {
+  for ( var i=0; i<6; i++) {
     Numbers= 'values'+i;
     elem = document.getElementById(Numbers);
     if (elem.checked == true) { sum += Number(elem.value); }
   }
   document.getElementById('totalcost').value = sum;
-  document.write("Your Total Is:" + totalcost.value);
+  document.write(" Total Is:" + totalcost.value);
   return false
 } 
